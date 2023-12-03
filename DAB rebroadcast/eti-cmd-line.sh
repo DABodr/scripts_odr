0c3b60e0f48820c7ca2e1107a1bf332a55cc103a
RSAP <46652797+DABodr@users.noreply.github.com>
2023-12-03 09:56:29 +0100

    Update eti-cmd-line.sh

--- DAB rebroadcast/eti-cmd-line.sh
+++ DAB rebroadcast/eti-cmd-line.sh
@@ -3,7 +3,7 @@
 # Boucle infinie pour relancer la commande si elle s'arrête.
 while true; do
     # Exécutez votre commande spécifique ici.
-    eti-cmdline-rtlsdr -D 5 -C 9D -Q | /home/pi/dab/eti-tools/eti2zmq -v -a -d -o "zmq+tcp://*:18081"
+    eti-cmdline-rtlsdr -D 5 -C 9D -Q | /home/$USER/dab/eti-tools/eti2zmq -v -a -d -o "zmq+tcp://*:18081"
 
     # Affiche un message et attend 5 secondes avant de relancer la commande.
     echo "La commande a été interrompue. Relance dans 5 secondes..."
