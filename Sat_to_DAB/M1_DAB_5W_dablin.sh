151cdcab8546cac25f9b0cdbb6e94259e0434770
RSAP <46652797+DABodr@users.noreply.github.com>
2024-05-06 23:37:04 +0200

    Update M1_DAB_5W_dablin.sh

--- Sat_to_DAB/M1_DAB_5W_dablin.sh
+++ Sat_to_DAB/M1_DAB_5W_dablin.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 
+
+dvbstream -f 11461000 -s 5780 8192 -p H -o | /home/$USER/dab/eti-tools/ts2na -s 12  -p 301 | /home/$USER/dab/eti-tools/na2ni | /home/$USER/dab/dablin/build/src/dablin_gtk
